e0fd520e4c50c2e59d28052cf835a15cd3fbed19
ischulz <ilias.schulz@gmail.com>
2018-01-17 19:49:43 -0800

    implemented BST, Hash and Set

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,48 @@
 var BinarySearchTree = function(value) {
+  let BST = Object.create(BinarySearchTree.prototype);
+  BST.left = null;
+  BST.right = null;
+  BST.value = value;
+  return BST;
+};
+
+BinarySearchTree.prototype.insert = function (value) {
+  if (value > this.value) {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  } else if (value < this.value) {
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+  if (this.value === value) {
+    return true;
+  }
+  if (this.left && this.left.contains(value)) {
+    return true;  
+  }
+  if (this.right && this.right.contains(value)) {
+    return true;
+  }
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  } 
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  } 
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,15 +6,27 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  let index = getIndexBelowMaxForKey(k, this._limit);
+  let obj = this._storage.get(index);
+  if (obj === undefined) {
+    obj = {};
+    this._storage.set(index, obj);
+  }
+  obj[k] = v;
 };
+
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  let index = getIndexBelowMaxForKey(k, this._limit);
+  let obj = this._storage.get(index);
+  if (obj !== undefined) {
+    delete obj[k];  
+  }
 };
 
 
